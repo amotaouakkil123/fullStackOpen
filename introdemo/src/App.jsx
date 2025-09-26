eefc302cc262852ea594a814d25910e6b0aefc52
amotaouakkil123 <adam.motaouakkil@mail.mcgill.ca>
2025-09-26 17:17:23 -0400

    Learning to write smarter jsx

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,24 +1,48 @@
-const Hello = (props) => {
-  console.log(props)
+const Header = (course) => {
+  console.log(course)
   return (
     <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      <p>{course.name}</p>
+    </div>
+  )
+}
+
+const Content = ({parts}) => {
+  console.log(parts)
+  return parts.map(part => (
+        <p>Name: {part.name}, Exercises: {part.exercises} </p>
+      ));
+}
+
+const Total = (a) => {
+  return (
+    <div>
+      <p>Exercises Total = {a.first + a.second + a.third}</p>
     </div>
   )
 }
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
-
+  const course = 'Half Stack application development'
+  const parts = [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+]
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name='Maya' age={26 + 10}/>
-      <Hello name={name} age={age}/>
-      <Hello/>
+      <Header name={course}/>
+      <Content parts={parts}/>
+      <Total first={parts[0].exercises} second={parts[1].exercises} third={parts[2].exercises}/>
     </div>
   )
 }
